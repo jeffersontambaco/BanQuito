caf9ccef9528be691f425723af7d0294d5c4957f
Jefferson Tambaco <jitambaco@espe.edu.ec>
2019-01-24 18:43:07 -0500

    DiseñoInterfaz
    JeffersonTambaco

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -11,8 +11,42 @@ import { InputTextModule } from 'primeng/inputtext';
 //Toda la logica del negocio
 export class PruebaComponent implements OnInit {
 
-  tipo_identificacion: any = "";
-  genero: any = "";
+  tipo_identificacionCliente: any = "";
+  generoCliente: any = "";
+  identificacionCliente: any = "";
+  apellidosCliente: any = "";
+  nombresCliente: any = "";
+  profesionCliente: any = "";
+  paisNacimientoCliente: any = "";
+  ciudadaniaCliente: any = "";
+  fechaRegistroCliente: any = "";
+  fechaNacimientoCliente: any = "";
+  paisResidenciaCliente: any = "";
+  provinciaResidenciaCliente: any = "";
+  ciudadResidenciaCliente: any = "";
+  callePrincipalCliente: any = "";
+  numCasaCliente: any = "";
+  calleSecundariaCliente: any = "";
+  referenciaCliente: any = "";
+  numeroCelularCliente: any = "";
+  numeroDomicilioCliente: any = "";
+  ccedulaReferencia: any = "";
+  apellidosReferencia: any = "";
+  nombreReferencia: any = "";
+  generoReferencia: any = "";
+  paisNacimientoReferencia: any = "";
+  estadoCivilReferencia: any = "";
+  parentescoCliente: any = "";
+  apellidoParentesco: any = "";
+  nombreParentesco: any = "";
+  paisNacimientoParentesco: any = "";
+  fechaNacimientoParentesco: any = "";
+  actividadEconomicaCliente: any = "";
+  ingresosMensualesCliente: any = "";
+  egresosMensualesCliente: any = "";
+  cargoPoliticoCliente: any = "";
+  fechaInicioPoliticoCliente: any = "";
+  fechaFinPoliticoCliente: any = "";
 
   tipo_iden: SelectItem[];
   gener: SelectItem[];
@@ -30,7 +64,14 @@ export class PruebaComponent implements OnInit {
 
   }
 
+
   ngOnInit() {
+  }
+
+  IngresoCliente() {
+    console.log("iden", this.tipo_identificacionCliente);
+    console.log("fecha", this.fechaRegistroCliente);
+    this.fechaNacimientoCliente=this.fechaRegistroCliente;
   }
 
 }
